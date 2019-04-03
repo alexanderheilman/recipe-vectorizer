cc86335d538d72a5d094b2168ffb60a0865fee75
Alex Heilman <alexanderlheilman@gmail.com>
2019-04-03 12:54:20 -0700

    Modified the search script

--- src/collect_search_results.py
+++ src/collect_search_results.py
@@ -10,27 +10,31 @@ from functions import *
 from sys import argv, exit
 import pymongo
 
- if __name__ == '__main__':
-    if len(argv) != 3:
-        print("Usage: python {} <keyword> <max_pages>".format(argv[0]))
+if __name__ == '__main__':
+    if len(argv) != 4:
+        print("Usage: python {} <keyword> <starting_page> <ending_page>".format(argv[0]))
         exit(1)
     keyword = argv[1]
-    max_pages = int(argv[2])
+    start_pg = int(argv[2])
+    end_pg = int(argv[3])
     # Connect to MongoClient and get collection handles
     mc = pymongo.MongoClient()
     db = mc['allrecipes']
     results_coll = db['search_results']
+    start_len = len(list(results_coll.find()))
     # Launch headless browser with selenium
     options = Options()
     options.headless = True
     capabilities = DesiredCapabilities().CHROME
     capabilities['pageLoadStrategy'] = 'none'
     browser = Chrome(desired_capabilities=capabilities, options=options)
-    for i in range(1,max_pages+1):
+    for i in range(start_pg, end_pg+1):
         url = create_search_url(keyword, i)
         browser.get(url)
         time.sleep(10)
         add_results_to_collection(browser, results_coll)
-        print('{} result pages collected.'.format(i))
+        print('Page {} collected.'.format(i))
     browser.quit()
-    print('Search complete!')
+    end_len = len(list(results_coll.find()))
+    diff = end_len - start_len
+    print('Search complete. {} new recipes found.'.format(diff))
